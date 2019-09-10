83f82466b62ea073bdeca577e5024cfdd45f6584
Ross Palmer <ross.palmer@gmail.com>
2019-09-10 16:10:42 +0100

    Initial framework for YAML parsing example

---
+++ Game.cpp
@@ -0,0 +1,103 @@
+#include "Game.h"
+#include <iostream>
+
+// Updates per milliseconds
+static double const MS_PER_UPDATE = 10.0;
+
+////////////////////////////////////////////////////////////
+Game::Game()
+	: m_window(sf::VideoMode(ScreenSize::s_height, ScreenSize::s_width, 32), "SFML Playground", sf::Style::Default)
+{
+	m_window.setVerticalSyncEnabled(true);
+
+	int currentLevel = 1;
+
+	// Invokes our level loader to read the YAML data - will generate an exception if level loading fails.
+	try
+	{
+		LevelLoader::load(currentLevel, m_level);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Level Loading failure." << std::endl;
+		std::cout << e.what() << std::endl;
+		throw e;
+	}
+}
+
+////////////////////////////////////////////////////////////
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Int32 lag = 0;
+
+	while (m_window.isOpen())
+	{
+		sf::Time dt = clock.restart();
+
+		lag += dt.asMilliseconds();
+
+		processEvents();
+
+		while (lag > MS_PER_UPDATE)
+		{
+			update(MS_PER_UPDATE);
+			lag -= MS_PER_UPDATE;
+		}
+		update(MS_PER_UPDATE);
+
+		render();
+	}
+}
+
+////////////////////////////////////////////////////////////
+void Game::processEvents()
+{
+	sf::Event event;
+	while (m_window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			m_window.close();
+		}
+		processGameEvents(event);
+	}
+}
+
+////////////////////////////////////////////////////////////
+void Game::processGameEvents(sf::Event& event)
+{
+	// check if the event is a a mouse button release
+	if (sf::Event::KeyPressed == event.type)
+	{
+		switch (event.key.code)
+		{
+		case sf::Keyboard::Escape:
+			m_window.close();
+			break;
+		case sf::Keyboard::Up:
+			// Up key was pressed...
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////
+void Game::update(double dt)
+{
+
+}
+
+////////////////////////////////////////////////////////////
+void Game::render()
+{
+	m_window.clear(sf::Color(0, 0, 0, 0));
+	m_window.display();
+}
+
+
+
+
+
